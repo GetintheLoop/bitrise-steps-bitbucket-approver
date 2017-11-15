376fa4c4c63248bb011ebb04b9feddab0ae75e83
Myroslav Pomazan <myroslav.pomazan@getintheloop.ca>
2017-11-15 09:22:29 -0800

    using python2

--- step.sh
+++ step.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pip2 install requests
-python "${THIS_SCRIPTDIR}/approvePR.py"
+python2 "${THIS_SCRIPTDIR}/approvePR.py"
